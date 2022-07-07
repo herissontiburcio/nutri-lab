645b7700d97d3b50763d3ab3355f8355fb8056e8
Herisson Tiburcio <herisson.tiburcio@gmail.com>
2022-07-07 17:04:34 -0300

    Improvement

---
+++ plataforma/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+path('pacientes/', views.pacientes, name='pacientes'),
+path('dados_paciente/', views.dados_paciente_listar, name="dados_paciente_listar"),
+]
